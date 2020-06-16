75cd61f0e4ea846b5a46a3567d476cec986ac81b
ainhoa <ainhoa.paredes@alumni.mondragon.edu>
2020-06-16 10:02:53 +0200

    modifico el carga fichero.R de nuevo

--- carga fichero.R
+++ carga fichero.R
@@ -1,3 +1,5 @@
+
+
 dim(muestra03)
 ## este fichero (muestra03) tiene una dimensión de 306085 filas y 3 columnas
 View(muestra03)
@@ -8,3 +10,13 @@ View(muestra03)
 dput(colnames(muestra03))
 colnames(muestra03)<-c("NifCif", "nombre", "genero" )
 View(muestra03)
+
+
+## variable poco informada ---------------------------
+library(tidyr)
+
+muestra03 <- muestra03%>%
+  filter(NifCif =!'' , !is.na(NifCif))
+
+### esta dando error pero yo haría eso asi para eliminar los registros poco informados de esa variable
+
